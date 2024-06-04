a9ef48e10c159496846ac5461c880146b0195c71
garima-76 <168435240+garima-76@users.noreply.github.com>
2024-06-04 06:28:20 +0000

    Create DevOpsExample.java

---
+++ DevOpsExample.java
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+public class DevOpsExample {
+ 
+     public static void main(String[] args) {
+ 
+               System.out.println("Welcome to Devops.");
+ 
+                                            }
+                          }
